3843b181ed9123167fba743092b0ce45c722c7ad
ls-madhu <madhusudhan.vinta@gmail.com>
2024-05-10 16:26:16 +0530

    feat: add links in footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,9 +1,17 @@
 <template>
   <footer class="bg-background w-full h-20 border-t border-muted">
     <div
-      class="flex items-center max-w-screen-xl w-full h-full font-sans-secondary text-xs font-medium text-foreground-secondary px-4 border-x border-muted mx-auto"
+      class="flex flex-col gap-4 sm:flex-row justify-center items-center sm:justify-between max-w-screen-xl w-full h-full font-sans-secondary text-xs font-medium text-foreground-secondary px-4 border-x border-muted mx-auto"
     >
-      &copy; 2024 threeory.com. All rights reserved.
+      <p>&copy; 2024 threeory.com. All rights reserved.</p>
+      <div class="flex flex-row gap-4">
+        <NuxtLink class="hover:underline hover:underline-offset-4" to="/"
+          >Terms & Conditions</NuxtLink
+        >
+        <NuxtLink class="hover:underline hover:underline-offset-4" to="/"
+          >Privacy Policy</NuxtLink
+        >
+      </div>
     </div>
   </footer>
 </template>
